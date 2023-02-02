7d602ab3b15f266e3b71f38307ffa4e72e8d1f86
Korchevskiy <korchevskiydmitriy2005@gmail.com>
2023-02-02 20:19:20 +0200

    created project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //        System.out.println("Hello world!");
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Input a number: ");
+//        int num = in.nextInt();
+//        System.out.println(Number(num));
+    }
+}
